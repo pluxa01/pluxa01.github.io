e88c27414e1743057351c83b334060a27205e070
Ravshan <ismailovravshan2008@gmail.com>
2023-08-16 09:37:23 +0500

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar';
-import Shop from './page/shop'
-import Cart from './page/cart'
-import ShopContextProvider from './context/ShopContext.jsx'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <ShopContextProvider>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path='/' element={<Shop />} />
-            <Route path='/cart' element={<Cart />} />
-          </Routes>
-        </Router>
-      </ShopContextProvider>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
